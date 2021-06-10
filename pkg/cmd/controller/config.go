8de1a6e97d7cfd5fb03df3118abb4e9ff077a6b6
Sebastian Laskawiec <slaskawi@redhat.com>
2021-06-10 11:47:53 +0200

    CSR approver for monitoring scraping

--- pkg/cmd/controller/config.go
+++ pkg/cmd/controller/config.go
@@ -4,10 +4,12 @@ var ControllerInitializers = map[string]InitFunc{
 	"openshift.io/namespace-security-allocation": RunNamespaceSecurityAllocationController,
 	"openshift.io/resourcequota":                 RunResourceQuotaManager,
 	"openshift.io/cluster-quota-reconciliation":  RunClusterQuotaReconciliationController,
+	"openshift.io/cluster-csr-approver":          RunCSRApproverController,
 }
 
 const (
 	infraClusterQuotaReconciliationControllerServiceAccountName  = "cluster-quota-reconciliation-controller"
+	infraClusterCSRApproverControllerServiceAccountName          = "cluster-csr-approver-controller"
 	infraNamespaceSecurityAllocationControllerServiceAccountName = "namespace-security-allocation-controller"
 	defaultOpenShiftInfraNamespace                               = "openshift-infra"
 )
